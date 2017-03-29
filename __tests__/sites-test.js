5323949a84a89adbcf4b765ce8c018748c46e8ee
vitaliysobur <workslon@gmail.com>
2017-03-29 21:20:30 +0300

    + getById()

--- __tests__/sites-test.js
+++ __tests__/sites-test.js
@@ -56,3 +56,30 @@ describe('sites.getByName', () => {
       });
   });
 });
+
+describe('sites.getById', () => {
+  it('returns site by given ID', () => {
+    var authPath = 'https://api-auth-eu.maxymiser.com';
+    var basePath = 'https://api-eu.maxymiser.com/v1';
+    var credentials = {
+      clientId: 'clientId',
+      clientSecret: 'clientSecret',
+      username: 'username',
+      password: 'password'
+    };
+    var authorize = auth.authorize(token, authPath, credentials);
+    var s = sites(basePath, authorize);
+    var expectedResult = {
+      id: 'MzIxMzM',
+      name: 'www.test.com',
+      lastIterationPublishDate: '',
+      lastIterationPublishHash: ''
+    };
+    var token;
+
+    return s.getById('MzIxMzM')
+      .then(site => {
+        expect(site).toEqual(expectedResult);
+      });
+  });
+});
