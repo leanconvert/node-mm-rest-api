549edf124f122fe525626b8cb322e1d399ea2fa8
vitaliysobur <workslon@gmail.com>
2017-03-30 13:53:45 +0300

    add req.js to contain generic request methods

---
+++ util/req.js
@@ -0,0 +1,43 @@
+module.exports = (authorize) => {
+  const get = (path) => {
+    return authorize()
+      .then(token => {
+        return new Promise((resolve, reject) => {
+          superagent
+            .get(path)
+            .set('Authorization', `Bearer ${token}`)
+            .end((err, res) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(res.body.items);
+              }
+            });
+        });
+      });
+  };
+
+  const put = (path, data) => {
+    return authorize()
+      .then(token => {
+        return new Promise((resolve, reject) => {
+          superagent
+            .put(path)
+            .send(data)
+            .set('Authorization', `Bearer ${token}`)
+            .end((err, res) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(res.body);
+              }
+            });
+        });
+      });
+  }
+
+  return {
+    get,
+    put
+  }
+}
