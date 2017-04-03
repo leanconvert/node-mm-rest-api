68dc86466a4f20b600f4dbfefbf9957a3e2b985a
vitaliysobur <workslon@gmail.com>
2017-04-04 00:26:28 +0300

    ~ req.js

--- util/req.js
+++ util/req.js
@@ -1,7 +1,7 @@
 const superagent = require('superagent');
 
 module.exports = (authorize) => {
-  const get = (path) => {
+  const get = (path, data) => {
     return authorize()
       .then(token => {
         return new Promise((resolve, reject) => {
@@ -12,7 +12,26 @@ module.exports = (authorize) => {
               if (err) {
                 reject(err);
               } else {
-                res.body.items ? resolve(res.body.items) : resolve(res.body);
+                var body = res.body;
+                var items = body.items;
+
+                if (items) {
+                  data && (items = items.map(item => {
+                    Object.keys(data).forEach(chunk => {
+                      item[chunk] = data[chunk];
+                    });
+
+                    return item;
+                  }));
+
+                  resolve(items);
+                } else {
+                  data && Object.keys(data).forEach(chunk => {
+                    body[chunk] = data[chunk];
+                  });
+
+                  resolve(res.body);
+                }
               }
             });
         });
