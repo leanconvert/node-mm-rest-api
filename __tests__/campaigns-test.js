edc72800cbe08f457773a0c67088dca44385aef7
vitaliysobur <workslon@gmail.com>
2017-04-04 20:06:30 +0300

    + campaigns.scripts

--- __tests__/campaigns-test.js
+++ __tests__/campaigns-test.js
@@ -200,3 +200,86 @@ describe('campaigns.actions.update()', () => {
     });
   });
 });
+
+describe('campaigns.scripts.get()', () => {
+  it('returns all campaign scripts', () => {
+    var authorize = auth.authorize(token, authPath, credentials);
+    var c = campaigns(basePath, authorize);
+    var expectedResult = [{
+      "campaignId": "MDA2MjYx",
+      "id":"NDMyNDMy",
+      "name":"Script 1",
+      "description":"My first script",
+      "content":"var pageViews = visitor.getData( 'Page viewed' );\nconsole.log( 'Number of page views: ' + pageViews);"
+    }];
+
+    return c.scripts.get({siteId: 'MzIxMzM', campaignId: 'MDA2MjYx'})
+      .then(scripts => {
+        expect(scripts).toEqual(expectedResult);
+      })
+  });
+});
+
+describe('campaigns.scripts.create()', () => {
+  it('creates new campaign script for the selected campaign', () => {
+    var authorize = auth.authorize(token, authPath, credentials);
+    var c = campaigns(basePath, authorize);
+    var expectedResult = {
+      "id":"NDMyNDQ0",
+      "name":"Rendering",
+      "description": "",
+      "content":"console.log( 'test' );"
+    };
+
+    return c.scripts.create({
+      siteId: 'MzIxMzM',
+      campaignId: 'MDA2MjYx',
+      name: 'Rendering',
+      content: "console.log( 'test' );"
+    }).then(scripts => {
+      expect(scripts).toEqual(expectedResult);
+    });
+  });
+});
+
+describe('campaigns.scripts.update()', () => {
+  it('updates campaign script by ID for the selected campaign', () => {
+    var authorize = auth.authorize(token, authPath, credentials);
+    var c = campaigns(basePath, authorize);
+    var expectedResult = {
+      "id":"NDMyNDMy",
+      "name":"Script 1",
+      "description": "My first script",
+      "content":"console.log( 'test' );"
+    };
+
+    return c.scripts.update({
+      siteId: 'MzIxMzM',
+      campaignId: 'MDA2MjYx',
+      scriptId: 'NDMyNDMy',
+      description: "My first script"
+    }).then(script => {
+      expect(script).toEqual(expectedResult);
+    });
+  });
+
+  it('updates campaign script by Name for the selected campaign', () => {
+    var authorize = auth.authorize(token, authPath, credentials);
+    var c = campaigns(basePath, authorize);
+    var expectedResult = {
+      "id":"NDMyNDMy",
+      "name":"Script 1",
+      "description": "My first script",
+      "content":"console.log( 'test' );"
+    };
+
+    return c.scripts.update({
+      siteId: 'MzIxMzM',
+      campaignId: 'MDA2MjYx',
+      scriptName: 'Script 1',
+      description: "My first script"
+    }).then(script => {
+      expect(script).toEqual(expectedResult);
+    });
+  });
+});
